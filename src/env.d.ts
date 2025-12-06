78fda34ec4d2465b1ebef8729a8f3385c4bd6c83
José Manuel Jiménez Rodríguez <josemajimenezrodriguez8@gmail.com>
2025-12-06 21:08:51 +0100

    Cambios para corregir el despliegue en deploy.yml

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,9 +2,4 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="@sanity/astro/module" />
-
-// SOLUCIÓN MANUAL: Esto le dice a TypeScript qué es "sanity:client"
-declare module "sanity:client" {
-  import { type SanityClient } from "@sanity/client";
-  export const sanityClient: SanityClient;
-}
+// El compilador de TypeScript ahora debe encontrar los tipos a través de la línea de arriba.
